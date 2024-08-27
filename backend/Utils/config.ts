dbf13fa7d9a71a80454eee1774045119eaea172a
Semyon <khram89@gmail.com>
2024-08-27 16:11:24 +0300

    toursApp

--- backend/Utils/config.ts
+++ backend/Utils/config.ts
@@ -1,8 +1,8 @@
 class Config{
     public webPort = 8080;
     public webHost = 'localhost';
-    public mySQLhost = 'localhost';
-    public mySQLuser = 'root';
+    public mySQLhost = 'mySQL';
+    public mySQLuser = 'user';
     public mySQLpassword = '12345678';
     public mySQLdb = 'vacations';
 }
